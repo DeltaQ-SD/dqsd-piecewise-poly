586080e43d27a0502a1cd1f379cff35f2e60e38c
9nine9nine9 <prog@jacobwalte.rs>
2023-09-04 17:56:20 +0100

    Contail all PWP files in dir, impl typeclasses

---
+++ src/DeltaQ/PWPs.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE TypeFamilies, FlexibleInstances, MultiParamTypeClasses #-}
+
+module DeltaQ.PWPs where
+
+import DeltaQ.Class
+
+import PWPs.IRVs
+
+instance (Ord a, Num a, Enum a, Fractional a) => ImproperRandomVariable (IRV a) where
+    type DelayModel (IRV a) = a
+    type ProbabilityModel (IRV a) = a
+
+    diracDelta = constructDelta
+    uniform0   = constructUniform
+    
+    perfection = diracDelta 0
+    bottom     = zeroPoly
+
+    tangibleMass = probMass
+
+instance (Ord a, Num a, Enum a, Fractional a) => Convolvable (IRV a) where
+    (<+>) = (PWPs.IRVs.<+>)
+
+instance (Ord a, Num a, Enum a, Fractional a) => NonConcurrentCombination a (IRV a) where
+    weightedChoice = probChoice
+
+instance (Ord a, Num a, Enum a, Fractional a) => ConcurrentCombination (IRV a) where
+    allToFinish   = PWPs.IRVs.allToFinish
+    firstToFinish = PWPs.IRVs.firstToFinish
